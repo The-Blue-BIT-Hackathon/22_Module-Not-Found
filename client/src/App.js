6f40c5b73427cd26eb0fa001fbbdfb93fb4980ba
ayush-dudhani <dudhaniayush@gmail.com>
2023-03-11 14:19:53 +0530

    Initialized Project

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
-function App() {
+const router = createBrowserRouter([
+    {
+        path : '/',
+        element : <div>Root Router</div>
+    },
+    {
+        path: '/register',
+        element : <div>Register Route</div>
+    },
+])
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
+    </>
+  )
 }
-
-export default App;
